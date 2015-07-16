d30a8036235bbc0e481484f86ad6b31331ab43b4
Atif Aziz <code@raboof.com>
2015-07-16 17:35:10 +0200

    Symbol package not needed

--- pack.cmd
+++ pack.cmd
@@ -2,5 +2,5 @@
 pushd "%~dp0"
 PowerShell -C "(type 'StackTraceParser.cs') -replace 'namespace\s+[a-z]+', 'namespace $rootnamespace$' | Out-File 'StackTraceParser.cs.pp'" ^
  && call build ^
- && nuget pack StackTraceParser.Source.nuspec -Symbol
+ && nuget pack StackTraceParser.Source.nuspec
 popd
