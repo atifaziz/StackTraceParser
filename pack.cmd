c198e2d85c303aacd8dc79b0735d293848c24509
Atif Aziz <code@raboof.com>
2015-07-16 17:39:54 +0200

    Encoding pp file in ASCII instead of UCS-2

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-PowerShell -C "(type StackTraceParser.cs) -replace 'namespace\s+[a-z]+', 'namespace $rootnamespace$' | Out-File StackTraceParser.cs.pp" ^
+PowerShell -C "(type StackTraceParser.cs) -replace 'namespace\s+[a-z]+', 'namespace $rootnamespace$' | Out-File -Encoding ascii StackTraceParser.cs.pp" ^
  && call build ^
  && nuget pack StackTraceParser.Source.nuspec
 popd
