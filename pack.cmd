94fe01e3bc37d11e6572f10d7c8913e98d8168e0
Daniel Marbach <daniel.marbach@openplace.net>
2020-11-20 19:00:42 +0100

    Modernise packaging & project as a result (#9)

--- pack.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd "%~dp0"
-PowerShell -C "(type StackTraceParser.cs) -replace 'namespace\s+[a-z]+', 'namespace $rootnamespace$' | Out-File -Encoding ascii StackTraceParser.cs.pp" ^
- && call build ^
- && nuget pack StackTraceParser.Source.nuspec
-popd
